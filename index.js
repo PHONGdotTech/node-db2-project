53cdaefb9d8dbe4b1c71cb3cf92d0f339c803861
Phong Yang <phongyang88@hotmail.com>
2020-02-18 15:17:41 -0600

    added base cars router for address of /api/cars

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const carsRouter = require("./cars/carsRouter.js")
 
 const server = express();
 
 server.use(express.json());
+server.use("/api/cars", carsRouter);
 
 const port = process.env.PORT || 5000;
 server.listen(port, ()=>{
